5da00c5db7f929927c0cc81e20ae63cdee0a8dba
Ruben kennedy <rubenkenne01@gmail.com>
2026-01-30 22:44:50 +0530

    Create Arrays_Find the Minimum Element in an Array.c
    Given an array of integers, determine the minimum element present in the array.
    
    Input Format
    
    First line: Integer N
    Second line: N space-separated integers
    Constraints
    
    1 ≤ N ≤ 10⁵
    −10⁹ ≤ A[i] ≤ 10⁹
    Output Format
    
    Print the minimum element of the array.

---
+++ Arrays_Find the Minimum Element in an Array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0; i<n; i++){
+        scanf("%d",&a[i]);
+    }
+    int max=a[0];
+    for(int i=1; i<n; i++){
+        if(a[i]<max){
+            max=a[i];
+        }
+    }
+    printf("%d", max);
+    return 0;
+}
+
+
+
+
