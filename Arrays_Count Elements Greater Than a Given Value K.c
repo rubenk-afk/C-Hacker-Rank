2f7feadd4b4dd4a188698464514b8f5e1a913fb0
Ruben kennedy <rubenkenne01@gmail.com>
2026-02-03 22:43:22 +0530

    Create Arrays_Count Elements Greater Than a Given Value K.c
    Given an array of integers and a value K, count how many elements in the array are strictly greater than K.
    
    Input Format
    
    First line: An integer N, representing the size of the array
    Second line: N space-separated integers
    Third line: An integer K
    Constraints
    
    1 ≤ N ≤ 10⁵
    −10⁹ ≤ A[i], K ≤ 10⁹
    Output Format
    
    Print a single integer representing the count of elements strictly greater than K.

---
+++ Arrays_Count Elements Greater Than a Given Value K.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for(int i=0; i<n; i++){
+        scanf("%d", &a[i]);
+    }
+    int num;
+    scanf("%d",&num);
+    int count=0;
+    for(int i=0; i<n; i++){
+        if(num<a[i]){
+            count++;
+        }
+    }printf("%d", count);
+    return 0;
+}
